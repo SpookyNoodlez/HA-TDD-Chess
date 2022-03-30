31d874684347c843b5ae5a2a38e4f68fb8a214e4
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2022-03-30 19:39:38 +0300

    Löpare kan inte gå till samma plats eller till ett random ställe

--- src/test/java/ax/ha/tdd/chess/engine/BishopTest.java
+++ src/test/java/ax/ha/tdd/chess/engine/BishopTest.java
@@ -2,7 +2,6 @@ package ax.ha.tdd.chess.engine;
 
 import ax.ha.tdd.chess.engine.pieces.Bishop;
 import ax.ha.tdd.chess.engine.pieces.PieceType;
-import ax.ha.tdd.chess.engine.pieces.Rook;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -80,44 +79,18 @@ public class BishopTest {
 
         Assertions.assertFalse(bishop.canMove(board,new Coordinates(3,5)));
     }
-
-    /*
-    @Test
-    public void MoveDownLeftUnhindered(){
-        Bishop bishop = new Bishop(PieceType.BISHOP,Player.WHITE,new Coordinates(7,2));
-        board.addPiece(bishop);
-
-        Assertions.assertTrue(bishop.canMove(board,new Coordinates(4,5)));
-    }
-    @Test
-    public void MoveUpRightUnhindered(){
-        Bishop bishop = new Bishop(PieceType.BISHOP,Player.WHITE,new Coordinates(4,4));
-        board.addPiece(bishop);
-
-        Assertions.assertTrue(bishop.canMove(board,new Coordinates(6,2)));
-    }
-    @Test
-    public void MoveDownRightUnhindered(){
-        Bishop bishop = new Bishop(PieceType.BISHOP,Player.WHITE,new Coordinates(0,2));
-        board.addPiece(bishop);
-
-        Assertions.assertTrue(bishop.canMove(board,new Coordinates(3,5)));
-    }*/
-
-    /*
     @Test
     public void FailToMoveToSameSpace(){
-        Rook rook = new Rook(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
-        board.addPiece(rook);
+        Bishop bishop = new Bishop(PieceType.BISHOP,Player.WHITE,new Coordinates(4,2));
+        board.addPiece(bishop);
 
-        Assertions.assertFalse(rook.canMove(board,new Coordinates(4,2)));
+        Assertions.assertFalse(bishop.canMove(board,new Coordinates(4,2)));
     }
     @Test
     public void FailToMoveToRandomSpace(){
-        Rook rook = new Rook(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
-        board.addPiece(rook);
+        Bishop bishop = new Bishop(PieceType.BISHOP,Player.WHITE,new Coordinates(4,2));
+        board.addPiece(bishop);
 
-        Assertions.assertFalse(rook.canMove(board,new Coordinates(6,5)));
+        Assertions.assertFalse(bishop.canMove(board,new Coordinates(6,5)));
     }
-    */
 }
