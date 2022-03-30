0631a8b4d177402508de0380267d3e86a92c6d70
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2022-03-30 19:37:38 +0300

    Fixade att springare hade fel piecetype i testen

--- src/test/java/ax/ha/tdd/chess/engine/KnightTest.java
+++ src/test/java/ax/ha/tdd/chess/engine/KnightTest.java
@@ -16,63 +16,63 @@ public class KnightTest {
 
     @Test
     public void OneUpTwoLeft(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(2,2)));
     }
     @Test
     public void OneUpTwoRight(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(6,2)));
     }
     @Test
     public void OneDownTwoLeft(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(2,4)));
     }
     @Test
     public void OneDownTwoRight(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(6,4)));
     }
     @Test
     public void TwoUpOneLeft(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(3,1)));
     }
     @Test
     public void TwoUpOneRight(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(5,1)));
     }
     @Test
     public void TwoDownOneLeft(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(3,5)));
     }
     @Test
     public void TwoDownOneRight(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,3));
         board.addPiece(knight);
 
         Assertions.assertTrue(knight.canMove(board,new Coordinates(5,5)));
     }
     @Test
     public void FailToMoveToRandomSpace(){
-        Knight knight = new Knight(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
+        Knight knight = new Knight(PieceType.KNIGHT,Player.WHITE,new Coordinates(4,2));
         board.addPiece(knight);
 
         Assertions.assertFalse(knight.canMove(board,new Coordinates(7,7)));
