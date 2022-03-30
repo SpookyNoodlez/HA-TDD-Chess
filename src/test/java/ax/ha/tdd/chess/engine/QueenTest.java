ff8ebc9b4bad49f811418066cf1d3129994c995f
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2022-03-30 19:43:50 +0300

    Drottningen passerar både tornets och löparens tester

--- src/test/java/ax/ha/tdd/chess/engine/QueenTest.java
+++ src/test/java/ax/ha/tdd/chess/engine/QueenTest.java
@@ -1,5 +1,6 @@
 package ax.ha.tdd.chess.engine;
 
+import ax.ha.tdd.chess.engine.pieces.Queen;
 import ax.ha.tdd.chess.engine.pieces.PieceType;
 import ax.ha.tdd.chess.engine.pieces.Queen;
 import org.junit.jupiter.api.Assertions;
@@ -14,38 +15,39 @@ public class QueenTest {
         board = Chessboard.startingBoard();
     }
 
+    //Rook tests
     @Test
     public void MoveLeftUnhindered(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,4));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
         board.addPiece(queen);
 
         Assertions.assertTrue(queen.canMove(board,new Coordinates(0,4)));
     }
     @Test
     public void MoveRightUnhindered(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,4));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
         board.addPiece(queen);
 
         Assertions.assertTrue(queen.canMove(board,new Coordinates(7,4)));
     }
     @Test
     public void MoveUpUnhindered(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,5));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,5));
         board.addPiece(queen);
 
         Assertions.assertTrue(queen.canMove(board,new Coordinates(4,2)));
     }
     @Test
     public void MoveDownUnhindered(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,2));
         board.addPiece(queen);
 
         Assertions.assertTrue(queen.canMove(board,new Coordinates(4,5)));
     }
     @Test
     public void FailToTakeOwnPiece(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(1,2));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,2));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(1,2));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -53,8 +55,8 @@ public class QueenTest {
     }
     @Test
     public void PieceInThePathGoingLeft(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(2,2));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,2));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(2,2));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -62,8 +64,8 @@ public class QueenTest {
     }
     @Test
     public void PieceInThePathGoingRight(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(5,2));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,2));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(5,2));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -71,8 +73,8 @@ public class QueenTest {
     }
     @Test
     public void PieceInThePathGoingUp(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,6));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,4));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,6));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -80,8 +82,8 @@ public class QueenTest {
     }
     @Test
     public void PieceInThePathGoingDown(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,4));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,3));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -89,8 +91,8 @@ public class QueenTest {
     }
     @Test
     public void CaptureEnemyDown(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,3));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.BLACK,new Coordinates(4,5));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,3));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.BLACK,new Coordinates(4,5));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -98,8 +100,8 @@ public class QueenTest {
     }
     @Test
     public void CaptureEnemyUp(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,5));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.BLACK,new Coordinates(4,3));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,5));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.BLACK,new Coordinates(4,3));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -107,8 +109,8 @@ public class QueenTest {
     }
     @Test
     public void CaptureEnemyLeft(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,5));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.BLACK,new Coordinates(1,5));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,5));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.BLACK,new Coordinates(1,5));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -116,8 +118,8 @@ public class QueenTest {
     }
     @Test
     public void CaptureEnemyRight(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,5));
-        Queen queen2 = new Queen(PieceType.ROOK,Player.BLACK,new Coordinates(7,5));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,5));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.BLACK,new Coordinates(7,5));
         board.addPiece(queen);
         board.addPiece(queen2);
 
@@ -125,16 +127,83 @@ public class QueenTest {
     }
     @Test
     public void FailToMoveToSameSpace(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,2));
         board.addPiece(queen);
 
         Assertions.assertFalse(queen.canMove(board,new Coordinates(4,2)));
     }
     @Test
     public void FailToMoveToRandomSpace(){
-        Queen queen = new Queen(PieceType.ROOK,Player.WHITE,new Coordinates(4,2));
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,2));
         board.addPiece(queen);
 
         Assertions.assertFalse(queen.canMove(board,new Coordinates(6,5)));
+    }
+
+    //Bishop tests
+    @Test
+    public void MoveUpLeftUnhindered(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
+        board.addPiece(queen);
+
+        Assertions.assertTrue(queen.canMove(board,new Coordinates(2,2)));
+    }
+    @Test
+    public void MoveUpLeftBlocked(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(3,3));
+        board.addPiece(queen);
+        board.addPiece(queen2);
+
+        Assertions.assertFalse(queen.canMove(board,new Coordinates(2,2)));
+    }
+    @Test
+    public void MoveDownLeftUnhindered(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(7,2));
+        board.addPiece(queen);
+
+        Assertions.assertTrue(queen.canMove(board,new Coordinates(4,5)));
+    }
+    @Test
+    public void MoveDownLeftBlocked(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(7,2));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(5,4));
+        board.addPiece(queen);
+        board.addPiece(queen2);
+
+        Assertions.assertFalse(queen.canMove(board,new Coordinates(4,5)));
+    }
+    @Test
+    public void MoveUpRightUnhindered(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
+        board.addPiece(queen);
+
+        Assertions.assertTrue(queen.canMove(board,new Coordinates(6,2)));
+    }
+    @Test
+    public void MoveUpRightBlocked(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(4,4));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(5,3));
+        board.addPiece(queen);
+        board.addPiece(queen2);
+
+        Assertions.assertFalse(queen.canMove(board,new Coordinates(6,2)));
+    }
+
+    @Test
+    public void MoveDownRightUnhindered(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(0,2));
+        board.addPiece(queen);
+
+        Assertions.assertTrue(queen.canMove(board,new Coordinates(3,5)));
+    }
+    @Test
+    public void MoveDownRightBlocked(){
+        Queen queen = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(0,2));
+        Queen queen2 = new Queen(PieceType.QUEEN,Player.WHITE,new Coordinates(1,3));
+        board.addPiece(queen);
+        board.addPiece(queen2);
+
+        Assertions.assertFalse(queen.canMove(board,new Coordinates(3,5)));
     }
 }
